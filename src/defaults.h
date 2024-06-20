359def0cbbb47bea131e3240c7d61e75959524bc
Jakob Schmid <jakobschmid25.12@gmail.com>
2024-06-20 20:48:43 +0200

    remove double set of default

--- src/defaults.h
+++ src/defaults.h
@@ -7,7 +7,6 @@ static void can_defaults() {
   canzero_set_command(input_board_command_NONE);
   canzero_set_assert_45V_system_online(bool_t_FALSE);
   canzero_set_sdc_status(sdc_status_OPEN);
-  canzero_set_assert_45V_system_online(bool_t_FALSE);
 
   canzero_set_assertion_fault(error_flag_OK);
 
